8d1ef3a69784afda74c411b42c17044133ca04e8
Felipe Zeba <felipe_zeba@outlook.com>
2024-02-08 19:04:59 -0600

    chore: export controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,7 @@
+export { DeveloperController } from './developer.controller';
+export { PublisherController } from './publisher.controller';
+export { PlatformController } from './platform.controller';
+export { GameController } from './game.controller';
+export { GenreController } from './genre.controller';
+export { UserController } from './user.controller';
+export { AuthController } from './auth.controller';
