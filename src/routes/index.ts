64b7524182f45fba5e3caea384f6f5fac532be6f
Felipe Zeba <felipe_zeba@outlook.com>
2024-02-08 19:08:11 -0600

    chore: export routers

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+export { userRouter } from './user.routes';
+export { gameRouter } from './game.routes';
+export { developerRouter } from './developer.routes';
+export { publisherRouter } from './publisher.routes';
+export { platformRouter } from './platform.routes';
+export { genreRouter } from './genre.routes';
