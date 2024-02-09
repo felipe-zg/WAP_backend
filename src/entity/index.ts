15a45284eb478c5a3c80420a653b34669d7b8523
Felipe Zeba <felipe_zeba@outlook.com>
2024-02-08 19:03:23 -0600

    chore: export entities

---
+++ src/entity/index.ts
@@ -0,0 +1,6 @@
+export { Developer } from './Developer.entity';
+export { Publisher } from './Publisher.entity';
+export { Platform } from './Platform.entity';
+export { Game } from './Game.entity';
+export { Genre } from './Genre.entity';
+export { User } from './User.entity';
